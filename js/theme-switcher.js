420a06ba0f36003530666baa559a0124153e24ec
AlexBlacksmith <unbloody.alex@gmail.com>
2022-06-24 18:52:22 +0500

    Added dark theme

--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -1,9 +1,30 @@
 const switcher = document.querySelector('#dark-mode');
+const styleLink = document.querySelector('#theme-link');
+const lightTheme = 'css/light.css';
+const darkTheme = 'css/dark.css';
 
-if (!localStorage['theme']) {
-    localStorage['theme'] = 'light';
-} 
+checkThemeState();
 
-switcher.addEventListener('click', () => {
-    document.body.classList.toggle('dark-body');
+switcher.addEventListener('click', (e) => {
+   changeTheme();
 });
+
+function changeTheme() {
+    let currentTheme = styleLink.getAttribute('href');
+    
+    if (currentTheme === lightTheme) {
+        currentTheme = darkTheme;
+        localStorage['theme'] = darkTheme;
+    } else {
+        currentTheme = lightTheme;
+        localStorage['theme'] = lightTheme;
+    }
+
+    styleLink.setAttribute('href', currentTheme);
+}
+
+function checkThemeState() {
+    if (localStorage['theme'] === darkTheme) {
+        styleLink.setAttribute('href', darkTheme);
+    }
+}
